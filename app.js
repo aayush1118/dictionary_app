1353e52120d9b949d006d1caf2d2fdc2c5496b0c
Aayush jain <aayushjain1118@gmail.com>
2021-01-23 00:35:57 +0530

    script

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ app.get('/api/:id', (req, res) => {
 if (process.env.NODE_ENV === 'production') {
 	console.log('active');
 	//set static folder
-	app.use(express.static('client/build'));
+	app.use(express.static('frontend/build'));
 	app.get('/', (req, res) => {
 		res.sendFile(
 			path.resolve(__dirname, 'frontend', 'build', 'index.html')
