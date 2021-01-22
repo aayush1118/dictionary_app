522290ffee5b924d5bfcdb0ae4d37a0df526a698
Aayush jain <aayushjain1118@gmail.com>
2021-01-23 00:28:38 +0530

    script

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 import Word from './components/Word';
 
 function App() {
-	console.log(process.env.PORT);
 	return (
 		<>
-			{/* <h1>{process.env.PORT}</h1> */}
+			<h1>{process.env.PORT}</h1>
 			<h1>test</h1>
-			{/* <Switch>
+			<Switch>
 				<Route exact path='/' component={List} />
 				<Route path='/view/:id' component={Word} />
-			</Switch> */}
+			</Switch>
 		</>
 	);
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,13 +6,11 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
-console.log(process.env.PORT);
-
 ReactDOM.render(
-	// <Provider store={store}>
-	// <BrowserRouter>
-	<App />,
-	// </BrowserRouter>
-	// </Provider>
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>,
 	document.getElementById('root')
 );
